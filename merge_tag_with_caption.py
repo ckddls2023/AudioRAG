3cc958570e5fe85f98e64df3a06cd15c1a7d93ef
ckddls2023 <ci2015.choi@snu.ac.kr>
2023-12-06 13:42:25 +0000

    Updated Instruction of LM caption seperation

--- merge_tag_with_caption.py
+++ merge_tag_with_caption.py
@@ -3,17 +3,26 @@ import subprocess
 
 query_json_files = [
   #'./data/json_files/AudioSet/val.json',
-  #'./data/json_files/Clotho/val.json',
-  #'./data/json_files/Clotho/train.json',
+  './data/json_files/Clotho/val.json',
+  './data/json_files/Clotho/train.json',
   './data/json_files/AudioSet/train.json',
 ]
-base_command = "~/dev/DL/llama.cpp/main -t 10 -ngl 32 -m ~/.cache/checkpoints/openorca-platypus2-13b.Q5_K_S.gguf \
--c 2048 --temp 0.7 --repeat_penalty 1.1 -n -1 -p \"### Instruction: Please break down complex 'sentence' to easy sentences with delimeter ','. \
-For example 'Birds chirp and a pop occurs before a man speaks' can be broken down into two sentences. 'Bird chirp, A man speaks'. \
-For another example 'A vehicle revving in the distance followed by a man shouting in the distance then a vehicle engine running idle before accelerating and driving off' can be broken down into two sentences. 'A vehicle revving, man shouting, driving off'. \
-For other example 'A man speaks and sweeps a surface' can be break down into two sentences. 'A man speaks, sweeps a surface'. \
-For simple example 'Blades spinning' cannot be break down. Just write 'Blades spinning'. \
-Please break down '{}'. Answer briefly in one sentence with comma. ### Response: \""
+base_command = "~/dev/DL/llama.cpp/main -t 10 -ngl 40 -m ~/.cache/checkpoints/openorca-platypus2-13b.Q5_K_S.gguf \
+-c 2048 --temp 0.7 --repeat_penalty 1.1 -n -1 -p \"### Instruction: Your role is break down complex 'sentence' to easy sentences with delimeter ','. \
+When you break down sentences, write sentences as short one. Drop sentence meaning less one. \
+This is examples of break down sentences. \
+'Birds chirp and a pop occurs before a man speaks' can be broken down into two sentences. 'Bird chirp, A man speaks'. \
+'A vehicle revving in the distance followed by a man shouting in the distance then a vehicle engine running idle before accelerating and driving off' can be broken down into two sentences. 'A vehicle revving, man shouting, driving off'. \
+'A man speaks and sweeps a surface' can be break down into two sentences. 'A man speaks, sweeps a surface'. \
+'A motor runs and fades as an adult man speaks' can be break down two sentences. 'A engine runs, a man speaks'. \
+'Repeating clicking gets faster until making a continues high pitch sound which slows and repeats' cannot break down. 'Clicks accelerate to a high-pitch and repeat'. \
+'An engine increases in speed as a horn honks and a man speaks' can be break down three sentences. 'accelerate, A horn sounds, A man speaks'.\
+'In the background, a group of people indistinctly chatter.' cannot break down. 'A group of people chatter'. \
+'A dog whimpers and bark' cannot break down. 'A dog barks'. \
+'fingers scrape on a metal tin, and have no apparent pattern' cannot break down. 'scrape metal noises'. \
+'An audience gives applause and a man laughs before speaking' can be break down three sentences. 'An audience applause, man laughs, man speaks'. \
+'Blades spinning' cannot break down. 'Blades spinning'. \
+Please break down '{}'. Answer results briefly in one sentence with comma. ### Response: '\""
 
 timeout_duration = 30  # For example, 30 seconds
 max_retries = 3  # Maximum number of retries
@@ -24,7 +33,9 @@ for json_file in query_json_files:
     num_captions = data["num_captions_per_audio"]
     for entry in data["data"]:
         if num_captions > 1:
-            caption = min(entry["caption"], key=len)  # Choose the shortest caption
+            sorted_captions = sorted(entry["caption"], key=len)
+            median_index = len(sorted_captions) // 2
+            caption = sorted_captions[median_index] # Choose median length
         else:
             caption = entry["caption"]  # Single caption
         command = base_command.format(caption)
@@ -45,9 +56,10 @@ for json_file in query_json_files:
         if result.returncode == 0:
             # Split the output to extract the part after "### Response:"
             parts = result.stdout.split("### Response:")
-            if len(parts) > 1:
-                tags = parts[1].strip().split(',')  # Remove any leading/trailing whitespace
+            if len(parts) == 2:
+                tags = parts[1].strip().replace('"', '').replace("'", "").split(',') # Remove any leading/trailing whitespace
                 entry["tag"] = tags
+                print(tags)
             else:
                 print(f"{entry['audio'] =} are not correctly processed")
                 print(result.stdout)
