5c2cf5ef58e8d9bf5aec7cbcfea351e236c373cf
ckddls2023 <ci2015.choi@snu.ac.kr>
2023-12-12 13:51:25 +0000

    Add LanguageBind to calculate similarity

--- generate_faiss_index.py
+++ generate_faiss_index.py
@@ -18,8 +18,7 @@ import psutil
 import numpy as np
 import ray
 from ray.data import from_items
-from laion_clap import CLAP_Module
-from laion_clap.training.data import get_audio_features, int16_to_float32, float32_to_int16
+from languagebind import LanguageBindAudio, LanguageBindAudioTokenizer, LanguageBindAudioProcessor
 
 # Get the total memory of the system
 num_gpus = torch.cuda.device_count()
@@ -45,54 +44,48 @@ query_json_files = [
 ]
 
 #index_file_path = "./data/index/index_pretrain.faiss"
-index_file_path = "./data/index/index_whole.faiss"
-index_exists = os.path.exists(index_file_path)
+audio_index_file_path = "./data/index/audio_index.faiss"
+text_index_file_path = "./data/index/text_index.faiss"
+index_exists = os.path.exists(audio_index_file_path)
 caption_file_path = "./data/index/big_kb_caption_wav_path.csv"
 caption_exists = os.path.exists(caption_file_path)
 
-def preprocess_waveform(record):
-    audio_cfg = {
-        "audio_length": 1024,
-        "clip_samples": 480000,
-        "mel_bins": 64,
-        "sample_rate": 48000,
-        "window_size": 1024,
-        "hop_size": 480,
-        "fmin": 50,
-        "fmax": 14000,
-        "class_num": 527,
-        "model_type": "HTSAT",
-        "model_name": "base"
-    }
-    waveform, sr = librosa.load(record["audio"], sr=48000, duration=record["duration"])
-    audio_waveform = int16_to_float32(float32_to_int16(waveform))
-    audio_waveform = torch.from_numpy(audio_waveform).float()
-    temp_dict = {}
-    temp_dict = get_audio_features(
-        temp_dict, audio_waveform, 480000,
-        data_truncating='fusion',
-        data_filling='repeatpad',
-        audio_cfg=audio_cfg,
-        require_grad=False,
-    )
-    return temp_dict
 
+model = LanguageBindAudio.from_pretrained('./checkpoint/')
+tokenizer = LanguageBindAudioTokenizer.from_pretrained('./checkpoint/')
+audio_process = LanguageBindAudioProcessor(model.config, tokenizer)
+# Duplicate the model for each CUDA device and move them to respective GPUs
+def preprocess_waveform(record):
+    #input_ids : torch.Size([1, 77])
+    #pixel_values : torch.Size([1, 3, 112, 1036])
+    data = audio_process(record["audio"], record['caption'], return_tensors='pt')
+    return data
 @ray.remote(num_gpus=1)  # Assign one GPU to this actor
 class AudioEmbeddingEncoder:
-    def __init__(self):
+    def __init__(self, model_ref):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.clap = CLAP_Module(enable_fusion=True)  # 615M
-        self.clap.load_ckpt()  # download the default pretrained checkpoint.
-        self.clap.eval()
-        self.clap.to(self.device)
+        self.model = model_ref.to(self.device)
+    def _prepare_batch(self, batch):
+        # Move each tensor in the batch to the specified device
+        return {k: v.to(self.device) for k, v in batch.items()}
 
     def encode_audio(self, batch):
-        outputs = self.clap.model.get_audio_embedding(batch)
-        return outputs
+        batch = self._prepare_batch(batch)
+        with torch.no_grad():
+            out = self.model(**batch)
+        return out.image_embeds
     
     def encode_text(self, batch):
-        outputs = self.clap.get_text_embedding(batch)
-        return outputs
+        batch = self._prepare_batch(batch)
+        with torch.no_grad():
+            out = self.model(**batch)
+        return out.text_embeds
+        
+    def encode_audio_text(self, batch):
+        batch = self._prepare_batch(batch)
+        with torch.no_grad():
+            out = self.model(**batch)
+        return out
     
 if not caption_exists:
     audio_filenames = []
@@ -109,6 +102,12 @@ if not caption_exists:
         data_filtered = [entry for entry in data["data"] if entry["duration"] <= 40] # Only under 40s
         audio_captions = audio_captions + [entry["caption"] for entry in data_filtered]
         audio_filenames = audio_filenames + [entry["audio"] for entry in data_filtered]
+    # Writing to the CSV file
+    with open(caption_file_path, mode='w', newline='', encoding='utf-8') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(["Caption", "Audio Filename"])  # Writing the header
+        for caption, filename in zip(audio_captions, audio_filenames):
+            csv_writer.writerow([caption, filename])
 else:
     audio_filenames = []
     audio_captions = []
@@ -120,18 +119,18 @@ else:
             audio_captions.append(row[0])   # Add caption to captions list
             audio_filenames.append(row[1]) # Add file path to file paths list
 
-    
+            
 def collate_fn(batch):
-    keys = batch.keys()
-    batch_length = len(batch["longer"])
-    transposed_batch = []
-    for i in range(batch_length):
-        data_point = {key: torch.tensor(batch[key][i]) for key in keys}
-        transposed_batch.append(data_point)
-    return transposed_batch
-        
+    #torch.Size([1, 77])
+    #torch.Size([1, 3, 112, 1036])
+    batch['input_ids'] = torch.tensor([input_id[0] for input_id in batch['input_ids']])
+    batch['attention_mask'] = torch.tensor([mask[0] for mask in batch['attention_mask']])
+    batch['pixel_values'] = torch.tensor(batch['pixel_values']).squeeze(1)
+    return batch
+
+    
 total_data_entries = 0
-embed_encoder_actors = [AudioEmbeddingEncoder.remote() for _ in range(num_gpus)]
+embed_encoder_actors = [AudioEmbeddingEncoder.remote(ray.put(LanguageBindAudio.from_pretrained('./checkpoint/'))) for _ in range(num_gpus)]
 print(f"Available resources : {num_cpus = } {num_gpus =}")
 if not index_exists:
     filtered_data = []
@@ -139,46 +138,47 @@ if not index_exists:
         with open(json_file, 'r') as file:
             data = json.load(file)
         for entry in data["data"]:
-            if 'author' in entry:
-                del entry["author"]
-            if 'description' in entry:
-                del entry["description"]
-            if 'download_link' in entry:
-                del entry["download_link"]
-            if 'file_name' in entry:
-                del entry["file_name"]
-            if 'tags' in entry:
-                del entry["tags"]
-            if 'href' in entry:
-                del entry["href"]
-            if 'category' in entry:
-                del entry["category"]
-            if 'title' in entry:
-                del entry["title"]
-            del entry["caption"]
+            keys_to_remove = ['author', 'description', 'category', 'download_link', 'file_name', 'href', 'category', 'title', 'tag', 'tags']
+            for key in keys_to_remove:
+                if key in entry:
+                    del entry[key]
+            if data["num_captions_per_audio"] > 1:
+                entry["caption"] = entry["caption"][0] # Only take first caption as retrieve text and encode
         data_filtered = [entry for entry in data["data"] if entry["duration"] <= 40] # Only under 40s
         filtered_data.extend(data_filtered)
     dataset = from_items(filtered_data)
     transformed_dataset = dataset.map(preprocess_waveform)
     result_refs = []
-    for i, batch in enumerate(transformed_dataset.iter_batches(batch_size=16, _collate_fn=collate_fn)):
+    for i, batch in enumerate(transformed_dataset.iter_batches(batch_size=24, _collate_fn=collate_fn)):
         actor = embed_encoder_actors[i % num_gpus]
-        result_refs.append(actor.encode_audio.remote(batch))
+        result_refs.append(actor.encode_audio_text.remote(batch))
     result_list = ray.get(result_refs) # Asynchronous execution, we synchronize after all results 
-    audio_embeds = torch.cat(result_list, dim=0)
+
+    audio_embeds = [out.image_embeds for out in result_list]
+    audio_embeds = torch.cat(audio_embeds, dim=0)
     audio_embeds = audio_embeds.detach().cpu().numpy()
-    dim = audio_embeds.shape[1] # B, H
+    audio_dim = audio_embeds.shape[1] # B, H
+    text_embeds = [out.text_embeds for out in result_list]
+    text_embeds = torch.cat(text_embeds, dim=0)
+    text_embeds = text_embeds.detach().cpu().numpy()
+    text_dim = text_embeds.shape[1] # B, H
     #nlist = 512 # 32~512, trade-off between search time, nprobe=32~128
     #quantizer = faiss.IndexFlatL2(dim)
     #index_cpu = faiss.IndexIVFFlat(quantizer, dim, nlist) # Introduce very erronoues results
     #training_samples = np.random.permutation(audio_embeds)[:29000] # Determine the number of training samples, typically ~10% of your dataset, 290k->29k
     #index_cpu.train(training_samples)
-    index_cpu = faiss.IndexFlatIP(dim)
-    index_cpu.add(audio_embeds)
-    faiss.write_index(index_cpu, index_file_path)
+    audio_index_cpu = faiss.IndexFlatIP(audio_dim)
+    audio_index_cpu.add(audio_embeds)
+    np.save("./data/index/audio_embeds.npy", audio_embeds)
+    faiss.write_index(audio_index_cpu, audio_index_file_path)
+    text_index_cpu = faiss.IndexFlatIP(text_dim)
+    text_index_cpu.add(text_embeds)
+    np.save("./data/index/text_embeds.npy", text_embeds)
+    faiss.write_index(text_index_cpu, text_index_file_path)
 else:
     print("reload faiss index from disk")
-    index_cpu = faiss.read_index(index_file_path)
+    audio_index_cpu = faiss.read_index(audio_index_file_path)
+    text_index_cpu = faiss.read_index(text_index_file_path)
 
 # Sanity check
 for json_file in retrieve_json_files:
@@ -187,7 +187,7 @@ for json_file in retrieve_json_files:
             data = json.load(file)
             data_filtered = [entry for entry in data["data"] if entry["duration"] <= 40] # Only under 40s
             total_data_entries += len(data_filtered)
-print(f"length of data samples {total_data_entries} and faiss index embedding {index_cpu.ntotal}, caption {len(audio_captions)}")
+print(f"length of data samples {total_data_entries} and faiss index embedding {audio_index_cpu.ntotal}, caption {len(audio_captions)}")
 
 @ray.remote
 class FaissSearcher:
@@ -200,7 +200,7 @@ class FaissSearcher:
         distances, indices = self.index.search(query_embed, self.top_k)
         return distances, indices
 
-search_actors = [FaissSearcher.remote(index_cpu, nprobe=16, top_k=5) for _ in range(int(num_cpus))]
+search_actors = [FaissSearcher.remote(audio_index_cpu, nprobe=16, top_k=5) for _ in range(int(num_cpus))]
 
 # Define a function to split query embeddings into batches
 def split_into_batches(embeddings, batch_size):
@@ -218,7 +218,7 @@ for json_file in query_json_files:
         data = json.load(file)
     for entry in data["data"]:
         # Remove specified keys
-        keys_to_remove = ['author', 'description', 'download_link', 'file_name', 'href', 'category', 'title', 'caption']
+        keys_to_remove = ['author', 'description', 'download_link', 'category', 'file_name', 'href', 'category', 'title', 'caption']
         for key in keys_to_remove:
             if key in entry:
                 del entry[key]
@@ -234,7 +234,7 @@ if not query_audio_embeds_exists:
     dataset = from_items(query_data)
     transformed_dataset = dataset.map(preprocess_waveform)
     result_refs = []
-    for i, batch in enumerate(transformed_dataset.iter_batches(batch_size=16, _collate_fn=collate_fn)):
+    for i, batch in enumerate(transformed_dataset.iter_batches(batch_size=24)):
         actor = embed_encoder_actors[i % num_gpus]
         result_refs.append(actor.encode_audio.remote(batch))
     result_list = ray.get(result_refs) # Asynchronous execution, we synchronize after all results 
@@ -245,7 +245,6 @@ else:
     print("Reload query audio embeds from numpy file")
     query_audio_embeds = np.load(query_audio_embeds_file)
 
-batch_size = 64
 query_batches = split_into_batches(query_audio_embeds, 64) # B => N, 64
 search_tasks = []
 for i, query_batch in enumerate(query_batches):
